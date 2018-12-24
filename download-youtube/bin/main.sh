a07d15c51a90e755607867984378851ba37e0be0
shinhwagk <shanghai_sl@hotmail.com>
2018-12-24 18:57:14 +0800

    Update main.sh

--- download-youtube/bin/main.sh
+++ download-youtube/bin/main.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-
-BASE=$(dirname $0);
-DOWNS="$BASE/../downloads";
+BASE=$(cd "$(dirname $0)"/../; pwd);
+DOWNS="$BASE/downloads";
+SUCCESS="$BASE/success";
+MOVIES="$BASE/movies";
 
 removeSuccess() {
   sudo docker ps -a --format '{{.Names}}' --filter ancestor='gk/youtube-dl' --filter 'exited=0' | while read line; do
     echo "$line download success."
-    echo $line >> success;
+    echo ${line:3} >> $SUCCESS;
     sudo docker rm $line;
   done
-  cat success | while read line; do
+  cat $SUCCESS | while read line; do
     sed -i "/${line}/d" $DOWNS;
   done
 }
 
 beenDowndOrDowning() {
-  local di=`sudo docker ps -a --format '{{.Names}}' --filter ancestor='gk/youtube-dl' --filter name=$1`
-  local bd=`cat success | grep -v grep | grep $1 1>/dev/null && echo 1 || echo 0`
+  local di=`sudo docker ps -a --format '{{.Names}}' --filter ancestor='gk/youtube-dl' --filter name=yd-$1`
+  local bd=`cat $SUCCESS | grep -v grep | grep $1 1>/dev/null && echo 1 || echo 0`
   if [ -z "$di" -a "$bd" == "0" ]; then
     echo 2 # no down.
   else
@@ -24,18 +25,26 @@ beenDowndOrDowning() {
   fi
 }
 
-main() {
-  removeSuccess;
+downFromDownloads() {
   cat $DOWNS | while read line; do
     p1=`echo $line | awk '{print $1}'`
     p2=`echo $line | awk '{print $2}'`
     if [ $(beenDowndOrDowning $p1) == 2 ]; then
-      echo "sudo docker run --name=${p1} --network=host -v `pwd`/$BASE/../movies:/youtube-dl -d gk/youtube-dl $p1 $p2"
-      sudo docker run --name=${p1} --network=host -v `pwd`/$BASE/../youtube:/youtube-dl -d gk/youtube-dl $p1 $p2
+      echo "sudo docker run --name=yd-${p1} --network=host -v $MOVIES:/youtube-dl -d gk/youtube-dl $p1 $p2"
+      sudo docker run --name=yd-${p1} --network=host -v $MOVIES:/youtube-dl -d gk/youtube-dl $p1 $p2
     else
       echo "$p1 downing."
     fi
-  done
+  done  
 }
 
-main
+appendDownloads() {
+  [ -z $1 ] || echo $1 >> $DOWNS
+}
+
+main() {
+  removeSuccess;
+  appendDownloads $1
+}
+
+main $@
