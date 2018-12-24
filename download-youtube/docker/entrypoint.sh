adc5dd83c7c1a168aa88e9439f9b84aeaea924b9
shinhwagk <shanghai_sl@hotmail.com>
2018-12-24 10:18:34 +0800

    Create entrypoint.sh

---
+++ download-youtube/docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+maxFormatCode() {
+  echo "youtube-dl --proxy socks5://127.0.0.1:1080 -F https://www.youtube.com/watch?v=${1} | sed '1,4d' | sort -n -k 1 -r | head -1 | awk '{print $1}'"
+  youtube-dl --proxy socks5://127.0.0.1:1080 -F https://www.youtube.com/watch?v=${1} | sed '1,4d' | sort -n -k 1 -r | head -1 | awk '{print $1}'
+}
+
+download() {
+  local fcode=$2
+  if [ $# == 1 ]; then
+    fcode=$(maxFormatCode $1)
+  fi
+  echo "youtube-dl -f $fcode --proxy socks5://127.0.0.1:1080 https://www.youtube.com/watch?v=$"
+  youtube-dl -f $fcode --proxy socks5://127.0.0.1:1080 https://www.youtube.com/watch?v=$1
+}
+
+download $@
